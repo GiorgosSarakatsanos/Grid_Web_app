5cc333defc609dfd994eb635054875fd2afed02c
Fix Store <support@fixstore.info>
2024-07-09 18:02:48 +0300

    Check versions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_flask_app',  # Replace with your project name
+    name='grid_web_app',  # Replace with your project name
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
